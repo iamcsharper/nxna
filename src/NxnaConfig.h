969710b665617479b0351d968f5487f3c822d9e1
bellatter@gmail.com <bellatter@gmail.com@1a2bb7b6-4c66-2606-fc19-086673613ebb>
2012-01-02 16:14:39 +0000

    iOS window is borderless again

--- src/NxnaConfig.h
+++ src/NxnaConfig.h
@@ -6,11 +6,7 @@
 typedef unsigned char byte;
 
 // are we using OpenGL ES?
-#ifdef NXNA_PLATFORM_APPLE_IPHONE
-#define USING_OPENGLES
-#elif defined NXNA_PLATFORM_IPHONE_SIMULATOR
-#define USING_OPENGLES
-#elif defined NXNA_PLATFORM_NACL
+#if defined NXNA_PLATFORM_APPLE_IOS || defined NXNA_PLATFORM_NACL
 #define USING_OPENGLES
 #endif
 
