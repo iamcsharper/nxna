e2257464532eb0b6d759c8db61ab266a8c852e07
bellatter@gmail.com <bellatter@gmail.com@1a2bb7b6-4c66-2606-fc19-086673613ebb>
2012-02-14 03:53:58 +0000

    stencil buffer wasn't working on some systems

--- trunk/src/Platform/OpenGlWindow.cpp
+++ trunk/src/Platform/OpenGlWindow.cpp
@@ -156,6 +156,7 @@ namespace Platform
 			SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8);
 			SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8);
 			SDL_GL_SetAttribute( SDL_GL_DEPTH_SIZE, 24);
+			SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 		}
 		SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
