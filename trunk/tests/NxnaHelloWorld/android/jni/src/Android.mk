d208ade74776d07432861ad137090bda3f022b42
bellatter <bellatter@1a2bb7b6-4c66-2606-fc19-086673613ebb>
2012-11-26 21:30:23 +0000

    Need to add this

---
+++ trunk/tests/NxnaHelloWorld/android/jni/src/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+# Build NXNA...
+NXNA_PATH := ../../../src
+include ../../../src/Android.mk
+
+# Build SDL
+#include ../../../lib/SDL_13/Android.mk
+
+LOCAL_MODULE := main
+LOCAL_MODULE_FILENAME := main
+SDL_PATH := ../SDL
+
+LOCAL_C_INCLUDES := $(NXNA_PATH)
+
+# Add your application source files here...
+LOCAL_SRC_FILES := ../../../Game1.cpp
+
+LOCAL_STATIC_LIBRARIES := nxna 
+LOCAL_SHARED_LIBRARIES := SDL2
+
+LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
+
+include $(BUILD_SHARED_LIBRARY)
