1a6a9d7f728263f28ca98095e0c9704d43666c73
bellatter <bellatter@1a2bb7b6-4c66-2606-fc19-086673613ebb>
2013-01-05 16:54:36 +0000

    added volume control to MediaPlayer

--- src/Media/MediaPlayer.h
+++ src/Media/MediaPlayer.h
@@ -10,9 +10,10 @@ namespace Media
 	class MediaPlayer
 	{
 		static Song* m_currentSong;
-		static int m_source;
+		static unsigned int m_source;
 		static unsigned int m_buffers[3];
 		static bool m_repeat;
+		static float m_volume;
 
 	public:
 		static void Play(Song* song);
@@ -22,6 +23,9 @@ namespace Media
 
 		static bool IsRepeating() { return m_repeat; }
 		static void IsRepeating(bool repeat) { m_repeat = repeat; }
+
+		static void SetVolume(float volume);
+		static float GetVolume() { return m_volume; }
 
 		// don't use this! This is just for the AudioManager to call!
 		static void Tick();
