6c7b7f0e72f6b3a2aebd931f0203cc15853d27e7
bellatter <bellatter@1a2bb7b6-4c66-2606-fc19-086673613ebb>
2014-04-27 21:50:35 +0000

    added NxnaUtils.h for extras that aren't technically part of XNA

---
+++ src/NxnaUtils.h
@@ -0,0 +1,8 @@
+#ifndef NXNAUTILS_H
+#define NXNAUTILS_H
+
+// This file is for "extra" parts of NXNA that aren't part of XNA, but are still handy.
+
+#include "Content/FileStream.h"
+
+#endif // NXNAUTILS_H
